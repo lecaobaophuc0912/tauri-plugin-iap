76634a7ebb2ab16ada227b4a84b7b198ba4fd2a9
Vladimir Pankratov <sollex.uz@gmail.com>
2025-07-29 21:59:22 +0500

    Basic plugin

---
+++ src/desktop.rs
@@ -0,0 +1,22 @@
+use serde::de::DeserializeOwned;
+use tauri::{plugin::PluginApi, AppHandle, Runtime};
+
+use crate::models::*;
+
+pub fn init<R: Runtime, C: DeserializeOwned>(
+  app: &AppHandle<R>,
+  _api: PluginApi<R, C>,
+) -> crate::Result<Iap<R>> {
+  Ok(Iap(app.clone()))
+}
+
+/// Access to the iap APIs.
+pub struct Iap<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> Iap<R> {
+  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    Ok(PingResponse {
+      value: payload.value,
+    })
+  }
+}
