c0969f609a5db7eef7ab62ce0662e59c12fe6908
Vladimir Pankratov <sollex.uz@gmail.com>
2025-07-30 01:02:57 +0500

    Fixed build on desktop

--- src/desktop.rs
+++ src/desktop.rs
@@ -15,26 +15,26 @@ pub struct Iap<R: Runtime>(AppHandle<R>);
 
 impl<R: Runtime> Iap<R> {
   pub fn initialize(&self) -> crate::Result<InitializeResponse> {
-    Err(crate::Error::from("IAP is not supported on desktop platforms"))
+    Err(crate::Error::from(std::io::Error::new(std::io::ErrorKind::Other, "IAP is not supported on desktop platforms")))
   }
 
   pub fn get_products(&self, _product_ids: Vec<String>, _product_type: String) -> crate::Result<GetProductsResponse> {
-    Err(crate::Error::from("IAP is not supported on desktop platforms"))
+    Err(crate::Error::from(std::io::Error::new(std::io::ErrorKind::Other, "IAP is not supported on desktop platforms")))
   }
 
   pub fn purchase(&self, _product_id: String, _offer_token: Option<String>) -> crate::Result<Purchase> {
-    Err(crate::Error::from("IAP is not supported on desktop platforms"))
+    Err(crate::Error::from(std::io::Error::new(std::io::ErrorKind::Other, "IAP is not supported on desktop platforms")))
   }
 
   pub fn restore_purchases(&self) -> crate::Result<RestorePurchasesResponse> {
-    Err(crate::Error::from("IAP is not supported on desktop platforms"))
+    Err(crate::Error::from(std::io::Error::new(std::io::ErrorKind::Other, "IAP is not supported on desktop platforms")))
   }
 
   pub fn get_purchase_history(&self) -> crate::Result<GetPurchaseHistoryResponse> {
-    Err(crate::Error::from("IAP is not supported on desktop platforms"))
+    Err(crate::Error::from(std::io::Error::new(std::io::ErrorKind::Other, "IAP is not supported on desktop platforms")))
   }
 
   pub fn acknowledge_purchase(&self, _purchase_token: String) -> crate::Result<AcknowledgePurchaseResponse> {
-    Err(crate::Error::from("IAP is not supported on desktop platforms"))
+    Err(crate::Error::from(std::io::Error::new(std::io::ErrorKind::Other, "IAP is not supported on desktop platforms")))
   }
 }
