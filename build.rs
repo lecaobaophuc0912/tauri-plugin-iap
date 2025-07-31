c63deeeb6dc197d19811778c5910547d292d9ef9
Vladimir Pankratov <sollex.uz@gmail.com>
2025-08-01 00:26:52 +0500

    Fixed build on android

--- build.rs
+++ build.rs
@@ -1,6 +1,3 @@
-#[cfg(target_os = "macos")]
-use swift_rs::SwiftLinker;
-
 const COMMANDS: &[&str] = &["initialize", "get_products", "purchase", "restore_purchases", "get_purchase_history", "acknowledge_purchase"];
 
 fn main() {
@@ -9,10 +6,12 @@ fn main() {
     .ios_path("ios")
     .build();
 
-  #[cfg(target_os = "macos")]
-  // swift-rs has a minimum of macOS 10.13
-  // Ensure the same minimum supported macOS version is specified as in your `Package.swift` file.
-  SwiftLinker::new("10.15")
-      .with_package("tauri-plugin-iap", "./macos/")
-      .link();
+  if std::env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() == "macos" {
+    // swift-rs has a minimum of macOS 10.13
+    // Ensure the same minimum supported macOS version is specified as in your `Package.swift` file.
+    #[cfg(target_os = "macos")]
+    swift_rs::SwiftLinker::new("10.15")
+        .with_package("tauri-plugin-iap", "./macos/")
+        .link();
+  }
 }
