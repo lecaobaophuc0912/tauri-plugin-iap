43f140b460cfd10262ad631d02b3cfd9aebf5bf2
Vladimir Pankratov <sollex.uz@gmail.com>
2025-08-07 12:54:34 +0200

    Disabled macos support

--- macos/Package.swift
+++ macos/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["tauri-plugin-iap"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Choochmeque/tauri-swift-runtime.git", from: "0.1.0")
+        .package(url: "https://github.com/Choochmeque/tauri-swift-runtime.git", from: "0.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
